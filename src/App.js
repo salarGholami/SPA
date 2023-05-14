2b0153607291525f5c0a5b7dfbdeb373a6c98dc5
Salar Gholami <salargholami021@gmail.com>
2023-05-14 15:01:57 +0330

    added Link Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 
@@ -6,9 +6,21 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about-us">About-us</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about-us" element={<AboutUs />} /> 
+          <Route path="/about-us" element={<AboutUs />} />
         </Routes>
       </BrowserRouter>
     </div>
